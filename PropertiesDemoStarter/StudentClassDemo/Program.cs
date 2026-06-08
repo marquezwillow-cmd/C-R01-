66fc5a147e996ddf145110da9ca7190a87e10983
marquezwillow-cmd <marquezwillow@gmail.com>
2026-06-07 20:11:03 -0600

    Add files via upload

---
+++ PropertiesDemoStarter/StudentClassDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            myStudent.Major = "Computer Programming";
+            myStudent.Score1 = 80;
+            myStudent.Score2 = 90;
+            myStudent.Score3 = 95;
+
+            Student myOtherStudent = new Student();
+            myOtherStudent.Major = "Art History";
+            myOtherStudent.Score1 = 90;
+            myOtherStudent.Score2 = 90;
+            myOtherStudent.Score3 = 95;
+
+            Console.WriteLine("My Major is {0} average is {1}", myStudent.Major, myStudent.Average);          
+            Console.WriteLine("The other student's major is {0} average is {1}.", myOtherStudent.Major, myOtherStudent.Average);
+        }
+    }
+}
---
+++ PropertiesDemoStarter/StudentClassDemo/Student.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassDemo
+{
+    class Student
+    {
+        //-------------------------------------
+        //Private Fields
+        //-------------------------------------
+
+        private int score1;
+        private int score2;
+        private int score3;
+
+        //-------------------------------------
+        //Constructors
+        //-------------------------------------
+
+        //default constructor
+        public Student()
+            : this("Number Pending", "TBD", "TBD", "Undeclared")
+        {
+        }
+
+        //single parameter constructor
+        public Student(string sID)
+            : this(sID, "TBD", "TBD", "Undeclared")
+        {
+        }
+
+        //constructor when we have student ID information and major
+        public Student(string sID, string firstName, string lastName, string maj)
+        {
+            StudentNumber = sID;
+            FirstName = firstName;
+            LastName = lastName;
+            Major = maj;
+        }
+
+        //constructor when we have student information and score information
+        public Student(string sID, string firstName, string lastName,
+            int s1, int s2, int s3, string maj)
+            : this(sID, firstName, lastName, maj)
+        {
+            score1 = s1;
+            score2 = s2;
+            score3 = s3;
+            CalcAverage();
+        }
+        //------------------------------------
+        //properties
+        //-----------------------------------
+
+        public string Major {get; set;}
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string StudentNumber { get; set; }
+
+        public int Score1
+        {
+            get { return score1; }
+            set { 
+                score1 = value; 
+                CalcAverage();
+            }
+        }
+      
+        public int Score2
+        {
+            get{ return score2; }
+            set { 
+                score2 = value;
+                CalcAverage() ;
+            }
+        }
+
+        public int Score3
+        {
+            get{ return score3; }
+            set { 
+                score3 = value;
+                CalcAverage() ;
+            }
+        }
+       
+        public double Average { get; private set; }
+
+        //-------------------------------------
+        //Instance Methods
+        //------------------------------------
+        public void CalcAverage()
+        {
+            Average = (score1 + score2 + score3) / 3.0;
+        }
+    }
+}
